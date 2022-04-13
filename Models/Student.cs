38e40b56b5540a1b63e57e2f92e33cca6c00cb91
kamilsoloducha <kamilsoloducha@gmail.com>
2022-04-13 07:09:07 +0200

    localization works

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,6 +4,7 @@ namespace MvcTest.Models;
 
 public class Student
 {
+    public long Id { get; set; }
     [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
     [MaxLength(20, ErrorMessage = "Name has to be shorter then 20")]
     [MinLength(1, ErrorMessage = "Name has to be longer then 3")]
